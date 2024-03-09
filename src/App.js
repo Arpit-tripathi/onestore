ca9d2b27b681b86adf1adb7fbfa51a3de53e48d0
Arpit-tripathi <tarpit501@gmail.com>
2024-03-09 19:48:19 +0530

    jjjj

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import { Counter } from './features/counter/Counter';
+import './App.css';
+
+import Home from './pages/Home';
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
+import Cart from './features/cart/Cart';
+import { createRoot } from "react-dom/client";
+import Checkout from './pages/Checkout';
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+import CartPage from './pages/CartPage';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element:
+      <Home></Home>
+    ,
+  },
+  {
+    path: "/Login",
+    element: <LoginPage></LoginPage>,
+  },
+  {
+    path: "/Signup",
+    element: <SignupPage></SignupPage>,
+  },
+  {
+    path: "/cart",
+    element: <CartPage></CartPage>,
+  },
+  {
+    path: "/checkout",
+    element: <Checkout></Checkout>,
+  },
+]);
+
+function App() {
+  return (
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
+  );
+}
+
+export default App;
